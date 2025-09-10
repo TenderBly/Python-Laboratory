8b54349d52f790207e8abb2f206d8db7d5992968
TenderBly <107112527+TenderBly@users.noreply.github.com>
2025-09-10 17:25:18 +0300

    Delete практична робота 1 directory

--- практична робота 1/PR1-3.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Введіть число від 2 до 8: "))
-
-# Верхня частина рисунка
-for i in range(1, n + 1):
-    print(str(n) * i)
-
-# Нижня частина рисунка
-for i in range(n - 1, 0, -1):
-    print(str(n) * i)
